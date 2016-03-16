0193c7182f6cd8adaacc370791acccf780a4897b
migo <migo_andres@yahoo.com>
2016-03-17 02:57:23 +0800

    added a note

--- src/dc2/Driver.java
+++ src/dc2/Driver.java
@@ -5,7 +5,7 @@ public class Driver {
 	public static void main(String args[]){
 		
 		View view1 = new View(1000, 700, "Productivity Tool");
-	
+		//kill me
 		System.out.println("putangain");
 	}
 
